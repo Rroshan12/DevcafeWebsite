692693640d101a4de682dbd686068854c194ef1f
Rroshan12 <grappleroshan7@gmail.com>
2024-03-27 19:22:23 +0545

    pm2 added

---
+++ pm2.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: 'docker-compose',
+        script: '/var/lib/jenkins/workspace/devcafe-website/start-docker-compose.sh',
+        autorestart: true,
+        watch: false,
+        log_date_format: 'YYYY-MM-DD HH:mm Z',
+        error_file: '/var/lib/jenkins/workspace/devcafe-website/pm2_error.log',
+        out_file: '/var/lib/jenkins/workspace/devcafe-website/pm2_out.log',
+        combine_logs: true,
+      },
+    ],
+  };
---
+++ start-docker-compose.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change directory to where your docker-compose.yml file is located
+cd /var/lib/jenkins/workspace/devcafe-website
+
+# Run docker-compose up command
+docker-compose up -d
