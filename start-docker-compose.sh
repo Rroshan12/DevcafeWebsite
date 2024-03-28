3939d3fc46fbc95084b87d023741d25b36898786
Rroshan12 <grappleroshan7@gmail.com>
2024-03-28 20:07:03 +0545

    added pm2 config on start up

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -3,5 +3,12 @@
 # Change directory to where your docker-compose.yml file is located
 cd /var/lib/jenkins/workspace/devcafe-website
 
+docker-compose up -d --build
 # Run docker-compose up command
-docker-compose up --scale api=2 --force-recreate
+docker-compose up --scale api=2 
+
+pm2 stop devcafe-website
+pm2 delete devcafe-website
+pm2 start pm2.config.js
+
+
